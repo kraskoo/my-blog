21c586e1bb6d7a5ebb0eab5659680588247b0f1c
kraskoo <kraskoo87@gmail.com>
2019-04-18 19:54:06 +0300

    Add db exports

---
+++ jsons/populate.cmd
@@ -0,0 +1,3 @@
+"C:\Program Files\MongoDB\Server\4.0\bin\mongoimport.exe" --db my-blog --collection users --file users.json --jsonArray
+"C:\Program Files\MongoDB\Server\4.0\bin\mongoimport.exe" --db my-blog --collection posts --file posts.json --jsonArray
+"C:\Program Files\MongoDB\Server\4.0\bin\mongoimport.exe" --db my-blog --collection comments --file comments.json --jsonArray
