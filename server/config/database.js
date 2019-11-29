d20406519de3d38c2bc523b10d8cb9c4a33d154a
kraskoo <kraskoo87@gmail.com>
2019-11-29 11:01:58 +0200

    fix indentation

--- server/config/database.js
+++ server/config/database.js
@@ -6,7 +6,7 @@ module.exports = (settings) => {
   mongoose.connect(settings.db, {
     useNewUrlParser: true,
     useCreateIndex: true,
-	useUnifiedTopology: true,
+    useUnifiedTopology: true,
     useFindAndModify: false
   });
   const db = mongoose.connection;
