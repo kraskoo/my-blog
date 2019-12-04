43da661c0d5d91e61cf400b3e05e53e5d03d7dc8
Krasimir Stefanov <kraskoo87@gmail.com>
2019-12-04 20:12:07 +0200

    1. Upgrade SortPostsPipe to be advanced 2. Fix last commit, shame :(

--- client/src/app/core/pipes/sort-posts.pipe.ts
+++ client/src/app/core/pipes/sort-posts.pipe.ts
@@ -3,22 +3,50 @@ import { ExtendedPost } from '../models/post.model';
 
 @Pipe({ name: 'sortPosts', pure: false })
 export class SortPostsPipe implements PipeTransform {
+  // format: posts | sortPosts: 'author._id,creationDate|desc,_id,title.length|asc,content'
   public transform(value: any, type: string) {
-    return (value as ExtendedPost[]).sort((a, b) =>
-      (this.getInnerProperty(a, type) as string).localeCompare(this.getInnerProperty(b, type) as string));
+    return (value as ExtendedPost[]).sort((a, b) => {
+      const aValues = this.getInnerProperty(a, type);
+      const bValues = this.getInnerProperty(b, type);
+      for (let i = 0; i < aValues.length; i++) {
+        const order = aValues[i].order;
+        const diff = order === 'asc' ?
+          aValues[i].value.localeCompare(bValues[i].value) :
+          bValues[i].value.localeCompare(aValues[i].value);
+        if (diff !== 0) {
+          return diff;
+        }
+      }
+
+      return 0;
+    });
   }
 
-  private getInnerProperty(obj: object, properties: string) {
+  private getInnerProperty(obj: object, properties: string): { value: string, order: string }[] {
+    const propsAndOrder = properties.split(',');
+    const values: { value: string, order: string }[] = [];
     let someValue = null;
-    const innerProperties = properties.split('.');
-    for (const property of innerProperties) {
-      if (someValue === null) {
-        someValue = obj[property];
-      } else {
-        someValue = someValue[property];
+    for (let propAndOrder of propsAndOrder) {
+      const pao = propAndOrder.split('|');
+      const prop = pao[0];
+      const order = pao.length > 1 ? pao[1] : 'asc';
+      if (order !== 'asc' && order !== 'desc') {
+        throw new Error('Order should be either asc or desc!');
       }
+
+      const innerProperties = prop.split('.');
+      for (const property of innerProperties) {
+        if (someValue === null) {
+          someValue = obj[property];
+        } else {
+          someValue = someValue[property];
+        }
+      }
+
+      values.push({ value: `${someValue}`, order });
+      someValue = null;
     }
 
-    return someValue;
+    return values;
   }
 }
